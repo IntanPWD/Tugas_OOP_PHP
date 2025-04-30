5ed9b070f484cceb0cffdacda2d3a2af80d9de34
TugasPWD4 <putu92067@gmail.com>
2025-04-30 21:14:38 +0700

    menambahkan tambahMakanan dan hitung total

--- Pesanan.php
+++ Pesanan.php
@@ -3,4 +3,15 @@
       class Pesanan {
           private $daftarPesanan = [];
 
+          public function tambahMakanan(MenuMakanan $makanan) {
+            $this->daftarPesanan[] = $makanan;
+        }
+        public function hitungTotal() {
+            $total = 0;
+            foreach ($this->daftarPesanan as $makanan) {
+                $total += $makanan->harga;
+            }
+            return $total;
+        }
+
           ?>
